e08585c7751d9e9a2d959d5b67f3d50b27f90152
josh-msn <joshua.maurer.mediaart@gmail.com>
2024-05-25 20:18:02 +0200

    All buttons and calculations working. Bugfixing open and chaining calculations not working completely for now

--- calculator/script.js
+++ calculator/script.js
@@ -1,40 +1,115 @@
-let inputNumber = "";
 let a = "";
 let b = "";
 let operator = "";
 let totalInput = "";
+let interimResult = "";
+let numFlag = false;
 
 document.addEventListener("DOMContentLoaded", function () {
-  const clickedButton = document.querySelectorAll("button");
+  const clickedButton = document.querySelectorAll(".num-button");
   clickedButton.forEach((clickedButton) => {
-    clickedButton.addEventListener("click", generateInput);
+    clickedButton.addEventListener("click", generateNumber);
   });
 });
 
-function generateInput() {
+document.addEventListener("DOMContentLoaded", function () {
+  const clickedButton = document.querySelectorAll(".op-button");
+  clickedButton.forEach((clickedButton) => {
+    clickedButton.addEventListener("click", handleNumbers);
+  });
+});
+
+document.getElementById("clear").addEventListener("click", clearCalculations);
+document.getElementById("delete").addEventListener("click", removeDigit);
+document.getElementById("equal").addEventListener("click", handleOperate);
+
+function updateDisplay(value) {
+  document.getElementById("output").innerHTML = value;
+}
+
+function clearCalculations() {
+  a = "";
+  b = "";
+  operator = "";
+  totalInput = "";
+  interimResult = "";
+  numFlag = false;
+  updateDisplay(totalInput);
+}
+
+function removeDigit() {
+  totalInput = totalInput.replace(/.$/, "");
+  updateDisplay(totalInput);
+}
+
+// saving input for later use in handleNumbers function
+function generateNumber() {
   const valueInput = this.value;
   totalInput = totalInput + valueInput;
-  console.log(totalInput);
+  updateDisplay(totalInput);
 }
 
-function add(a, b) {
-  const result = a + b;
-  return result;
+// saving the inputs to the variables a and b + saving the operator in variable
+function handleNumbers() {
+  if (!numFlag) {
+    operator = this.value;
+    a = parseFloat(totalInput);
+    totalInput = "";
+    numFlag = true;
+  } else {
+    //console.log(`Zwischen: '${interimResult}'`);
+    interimResult = handleOperate();
+    a = interimResult;
+    b = "";
+    numFlag = true;
+  }
 }
 
-function substract(a, b) {
-  const result = a - b;
-  return result;
+function handleOperate() {
+  b = parseFloat(totalInput);
+  totalInput = "";
+  numFlag = false;
+  return operate(a, b, operator);
 }
 
-function multiply(a, b) {
-  const result = a * b;
-  return result;
-}
+function operate(a, b, operator) {
+  let result = 0;
+  switch (operator) {
+    case "+":
+      result = add(a, b);
+      return result;
+    case "-":
+      result = substract(a, b);
+      return result;
+    case "*":
+      result = multiply(a, b);
+      return result;
+    case "/":
+      result = divide(a, b);
+      return result;
+  }
 
-function divide(a, b) {
-  const result = a / b;
-  return result;
-}
+  function add(a, b) {
+    const result = a + b;
+    updateDisplay(result);
+    return result;
+  }
 
-function operate(a, b) {}
+  function substract(a, b) {
+    const result = a - b;
+    updateDisplay(result);
+    return result;
+  }
+
+  function multiply(a, b) {
+    const result = a * b;
+    updateDisplay(result);
+    return result;
+  }
+
+  function divide(a, b) {
+    const result = a / b;
+    updateDisplay(result);
+    return result;
+  }
+}
