c95700e6f2549807da9a88cfaaf1a39dc34c8029
josh-msn <joshua.maurer.mediaart@gmail.com>
2024-05-26 17:48:46 +0200

    Working calculator, without keyboard support and before refacturing the code

--- calculator/script.js
+++ calculator/script.js
@@ -3,7 +3,9 @@ let b = "";
 let operator = "";
 let totalInput = "";
 let interimResult = "";
+let result = "";
 let numFlag = false;
+let euqalFlag = false;
 
 document.addEventListener("DOMContentLoaded", function () {
   const clickedButton = document.querySelectorAll(".num-button");
@@ -21,29 +23,42 @@ document.addEventListener("DOMContentLoaded", function () {
 
 document.getElementById("clear").addEventListener("click", clearCalculations);
 document.getElementById("delete").addEventListener("click", removeDigit);
-document.getElementById("equal").addEventListener("click", handleOperate);
+document.getElementById("equal").addEventListener("click", handleEqual);
 
 function updateDisplay(value) {
   document.getElementById("output").innerHTML = value;
 }
 
+// checking if decimal point is used and deactivating the button
+function checkDecimal(value) {
+  if (value.includes(".")) {
+    document.getElementById("dot").disabled = true;
+  } else {
+    document.getElementById("dot").disabled = false;
+  }
+}
+
 function clearCalculations() {
   a = "";
   b = "";
+  result = "";
   operator = "";
   totalInput = "";
   interimResult = "";
   numFlag = false;
+  equalFlag = false;
   updateDisplay(totalInput);
 }
 
 function removeDigit() {
   totalInput = totalInput.replace(/.$/, "");
+  checkDecimal(totalInput);
   updateDisplay(totalInput);
 }
 
 // saving input for later use in handleNumbers function
 function generateNumber() {
+  checkDecimal(totalInput);
   const valueInput = this.value;
   totalInput = totalInput + valueInput;
   updateDisplay(totalInput);
@@ -51,35 +66,51 @@ function generateNumber() {
 
 // saving the inputs to the variables a and b + saving the operator in variable
 function handleNumbers() {
-  if (!numFlag) {
-    operator = this.value;
-    a = parseFloat(totalInput);
-    totalInput = "";
-    numFlag = true;
+  if (!(totalInput === "")) {
+    if (!numFlag) {
+      operator = this.value;
+      a = parseFloat(totalInput);
+      totalInput = "";
+      numFlag = true;
+    } else {
+      if (!equalFlag) {
+        interimResult = handleOperate();
+        operator = this.value;
+        a = interimResult;
+      } else {
+        operator = this.value;
+      }
+    }
+  }
+}
+
+function roundToX(num, decimals) {
+  return +(Math.round(num + "e" + decimals) + "e-" + decimals);
+}
+
+function handleEqual() {
+  if (totalInput === "" && operator === "") {
+    console.log("nope");
   } else {
-    //console.log(`Zwischen: '${interimResult}'`);
-    interimResult = handleOperate();
-    a = interimResult;
-    b = "";
-    numFlag = true;
+    const result = handleOperate();
+    a = result;
+    equalFlag = true;
   }
 }
 
 function handleOperate() {
   b = parseFloat(totalInput);
   totalInput = "";
-  numFlag = false;
   return operate(a, b, operator);
 }
 
 function operate(a, b, operator) {
-  let result = 0;
   switch (operator) {
     case "+":
       result = add(a, b);
       return result;
     case "-":
-      result = substract(a, b);
+      result = subtract(a, b);
       return result;
     case "*":
       result = multiply(a, b);
@@ -88,27 +119,37 @@ function operate(a, b, operator) {
       result = divide(a, b);
       return result;
   }
+}
 
-  function add(a, b) {
-    const result = a + b;
+//mathematical operations
+function add(a, b) {
+  let result = a + b;
+  result = roundToX(result, 4);
+  updateDisplay(result);
+  return result;
+}
+
+function subtract(a, b) {
+  let result = a - b;
+  result = roundToX(result, 4);
+  updateDisplay(result);
+  return result;
+}
+
+function multiply(a, b) {
+  let result = a * b;
+  result = roundToX(result, 4);
+  updateDisplay(result);
+  return result;
+}
+
+function divide(a, b) {
+  if (b === 0) {
+    result = "nope";
     updateDisplay(result);
-    return result;
-  }
-
-  function substract(a, b) {
-    const result = a - b;
-    updateDisplay(result);
-    return result;
-  }
-
-  function multiply(a, b) {
-    const result = a * b;
-    updateDisplay(result);
-    return result;
-  }
-
-  function divide(a, b) {
-    const result = a / b;
+  } else {
+    let result = a / b;
+    result = roundToX(result, 4);
     updateDisplay(result);
     return result;
   }
